849a85ae69cd76a508175391c4195f78daa01512
Ayoze Vera Arbelo <ayo.verad@gmail.com>
2018-01-10 01:14:11 +0000

    store configuration moved to app.js and app component created as root component, also debug method implemented to subscribe to the store

--- src/app.js
+++ src/app.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+import { ConnectedRouter, routerMiddleware, routerReducer } from 'react-router-redux'
+import thunkMiddleware from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import { ExampleContainer } from './containers'
-import configureStore from './store/configure'
+import reducers from './reducers'
 import './styles/main.scss'
 
-const store = configureStore()
 const history = createHistory()
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <Switch>
-          <Route exact path="/" component={ExampleContainer} />
-        </Switch>
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  document.getElementById('app')
+const store = createStore(
+  combineReducers({ ...reducers, router: routerReducer }),
+  applyMiddleware(thunkMiddleware, routerMiddleware(history))
 )
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    // Uncomment the line below complete line show store changes as console.logs
+    this.debug()
+  }
+
+  debug() {
+    window.store = store
+    store.subscribe(() => console.log(store.getState()))
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>
+            <Switch>
+              <Route exact path="/" component={ExampleContainer} />
+            </Switch>
+          </div>
+        </ConnectedRouter>
+      </Provider>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
