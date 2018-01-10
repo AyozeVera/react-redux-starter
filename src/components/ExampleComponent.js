83f4e989dcb31d1966f1b3117f7c80932341c597
Ayoze Vera Arbelo <ayo.verad@gmail.com>
2018-01-10 01:15:47 +0000

    example component and container updated with store changes trys

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class ExampleComponent extends Component {
+  static get propTypes() {
+    return {
+      onBtnClick: PropTypes.func.isRequired,
+      content: PropTypes.node,
+    }
+  }
+
+  handleOnClick(event) {
+    event.preventDefault()
+    this.props.onBtnClick()
+  }
+
   render() {
     return (
-      <h1>Example Component</h1>
+      <div>
+        <h1>Example Component</h1>
+        <button onClick={(event) => this.handleOnClick(event)}>Change my value! - {this.props.content ? this.props.content : ''}</button>
+      </div>
     )
   }
 }
